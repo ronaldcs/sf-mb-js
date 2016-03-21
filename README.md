53326131ad05bf819d6457d7e94077c55f8fad0e
Ron Sogueco <ron.sogueco@ccur.com>
2016-03-20 23:42:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+You are given a function 'secret()' that accepts a single integer parameter and returns an integer. In your favorite programming language, write a command-line program that takes one command-line argument (a number) and determines if the secret() function is additive [secret(x+y) = secret(x) + secret(y)], for all combinations x and y, where x and y are all prime numbers less than the number passed via the command-line argument.  Describe how to run your examples. Please generate the list of primes without using built-in functionality.
+
+How to run:
+
+1. Visit https://repl.it/ByGK/2. Note that this link is essentially a duplicate of the coding-exercise.js file.
+2. Click on the "run" button.
+3. In the console, run the following command:
+
+```javascript
+ce.test(<param>);
+```
+
+Where `<param>` is the integer parameter of your choosing.
---
+++ coding-exercise.js
@@ -0,0 +1,78 @@
+/**
+ * Secret function.
+ * @param  [int] i - Accept an integer.
+ * @return [int]
+ */
+var secret = function (i) {
+  return i;
+}
+var CodingExercise = (function () {
+  /**
+   * Constructor
+   */
+  function CodingExercise() {
+  }
+  /**
+   * Get all prime numbers given an upper limit.
+   * @param  [int] $n - Upper limit.
+   * @return [int[]]
+   */
+  function getPrimes(n) {
+    /* Implement https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes */
+    var arr = [], primes = []; /* arr is a temporary array to mark non-prime numbers */
+    for(var i = 2; i <= n; i++) { /* Start with 2 */
+      if(arr[i] !== false) {
+        primes.push(i); /* Elements that are not false */
+        for(var j = Math.pow(i, 2); j <= n; j += i) { /* Mark multiples of $i as false (i.e. non-prime) */
+          arr[j] = false;
+        }
+      }
+    }
+    return primes;
+  }
+  /**
+   * Get unique pairs of an array the imperative way.
+   * @param  [int[]] $arr - Array of ints.
+   * @return [int[int[]]]
+   */
+  function getPairs(arr) {
+    return arr.reduce(function (cur, nxt, ind, arr) {
+      for(ind; ind < (arr.length - 1); ind++) {
+        cur.push([nxt, arr[ind + 1]]); /* For each value after ind, pair consecutive ind's */
+      }
+      return cur;
+    }, []); /* Start with an array */
+  }
+  /**
+   * Is the secret function additive?
+   * @param  [int] x - First int.
+   * @param  [int] y - Second int.
+   * @return boolean
+   */
+  function isAdditive(x, y) {
+    return secret(x + y) === secret(x) + secret(y);
+  }
+  /**
+   * Tests all unique pairs of prime numbers to determine if the secret function is additive. Print out the object for info.
+   * @param  [int] n - Test all prime numbers up to this number.
+   * @return [boolean]
+   */
+  CodingExercise.prototype.areAllPairsAdditive = function(n) {
+    var me = this;
+    me._limit = n;
+    me._primes = getPrimes(me._limit);
+    me._pairs = getPairs(me._primes);
+    if(parseInt(me._limit) === me._limit) {
+      for(var i = 0; i < me._pairs.length; i++) {
+        if(isAdditive(me._pairs[i][0], me._pairs[i][1]) !== true) { /* Iterate until a pair is not additive */
+          console.log("secret() is not additive");
+          return false;
+        }
+      }
+    }
+    console.log("secret() is additive");
+    return true;
+  }
+  return CodingExercise;
+})();
+var ce = new CodingExercise();
